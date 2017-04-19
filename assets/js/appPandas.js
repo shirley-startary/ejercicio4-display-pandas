b525a351ba3540fbbf92f609214f6729697e237f
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-18 19:14:31 -0500

    creando la funcion para ocultar el texto de Origen

--- assets/js/appPandas.js
+++ assets/js/appPandas.js
@@ -1,12 +1,14 @@
 var botonesCerrar=document.getElementsByClassName("cerrar");
 var botonRestaurar = document.getElementById("restaurar");
 var botonExtincion = document.getElementById("extincion");
+var botonOrigen = document.getElementById("origen");
 
 for (var i=0;i<botonesCerrar.length;i++){
   botonesCerrar[i].addEventListener("click", function(){cerrarImagenes(this)});
 }
 botonRestaurar.addEventListener("click",restaurarFotos);
 botonExtincion.addEventListener("click",ocultarTextoExtincion);
+botonOrigen.addEventListener("click",ocultarTextoOrigen);
 
 function cerrarImagenes(esto){
   //en esta funcion hacemos que se oculte cada imagen.
@@ -27,9 +29,20 @@ function ocultarTextoExtincion(){
   //esta funcion oculta y muestra el texto de extincion
   var textoExtincion = document.getElementById("textoExtincion");
   console.log(textoExtincion);
-  if(textoExtincion.style.visibility=="hidden"){
+  if(textoExtincion.style.visibility==="hidden"){
     textoExtincion.style.visibility="visible"
   }else {
     textoExtincion.style.visibility="hidden"
+  }
+}
+
+function ocultarTextoOrigen(){
+  //esta funcion oculta y muestra el texto de Origen
+  var textoOrigen = document.getElementById("textoOrigen");
+  console.log(textoOrigen);
+  if(textoOrigen.style.visibility==="hidden"){
+    textoOrigen.style.visibility="visible"
+  }else {
+    textoOrigen.style.visibility="hidden"
   }
 }
